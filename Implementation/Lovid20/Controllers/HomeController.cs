e8528a1de6c0d91d009c1e06989485cb9e7b4df3
Ajla Dzaferbegovic <56268385+adzaferbeg1@users.noreply.github.com>
2020-05-27 11:22:23 +0200

    Dodan Register i Login view

--- Implementation/Lovid20/Controllers/HomeController.cs
+++ Implementation/Lovid20/Controllers/HomeController.cs
@@ -23,11 +23,12 @@ namespace Lovid20.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+
+        public IActionResult Login()
+
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
